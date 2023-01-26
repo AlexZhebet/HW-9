2f39a45002a0c3f6b5197290a417bd293b1a56c7
AlexZhebet <115915899+AlexZhebet@users.noreply.github.com>
2023-01-26 08:38:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // HW №9 
 const array = [1, 2, 3, 4, 5, 6, 7] 
  
-function removeElement(array, item) { 
-    array.splice(item, 1) 
-} 
-removeElement(array, 5) 
-console.log(array) 
- 
-// const array = [1, 2, 3, 4, 5, 6, 5, 5, 5, 5, 5, 5, 7] 
+
 function removeElement(array, item) { 
     while (array.includes(item)) { 
         let arrLet = array.indexOf(item) 
